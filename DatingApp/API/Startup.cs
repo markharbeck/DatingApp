5503f3cd1e59f7c05bcd6fcd6bfadb437178d30e
Mark Harbeck <mark.harbeck@hotmail.com>
2021-02-22 15:52:53 +1000

    End of section 3

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -25,7 +25,7 @@ namespace API
       _config = config;
 
     }
-    
+
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
@@ -40,6 +40,9 @@ namespace API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
+
+      services.AddCors();
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +58,11 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("https://localhost:4200")
+                  );
 
       app.UseAuthorization();
 
